1b09bbf83c3fd9c7c14ed377329728db9dcc3d2f
Tom <thomasjackfairhall@gmail.com>
2022-07-16 20:54:49 +1000

    syntax fixes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 import csv
-
 from PiicoDev_BME280 import PiicoDev_BME280
 from PiicoDev_VEML6030 import PiicoDev_VEML6030
 
@@ -11,19 +10,21 @@ bme280 = PiicoDev_BME280()
 veml6030 = PiicoDev_VEML6030()
 
 # take an initial altitude reading
-zeroAlt = bme280.altitude()
+zero_alt = bme280.altitude()
 
 # read and assign the sesnor values
-tempC, presPa, humRH = bme280.values()
-lightLx = veml6030.read()
+temp_C, pres_Pa, hum_RH = bme280.values()
+light_Lx = veml6030.read()
 
 # convert air pressure Pascals -> hPa
-presHPa = presPa / 100
+pres_HPa = pres_Pa / 100
 
+# get the current date and time
 now = datetime.now()
 
+# open, or create a file in append mode and write the envrio varaibles
 with open('data.csv', 'a', newline='') as file:
     writer = csv.writer(file)     
-    writer.writerow([now.strftime("%Y-%m-%d"), now.strftime("%H:%M:%S"), str(tempC), str(presHPa), str(humRH), str(lightLx)])
+    writer.writerow([now.strftime("%Y-%m-%d"), now.strftime("%H:%M:%S"), str(temp_C), str(pres_HPa), str(hum_RH), str(light_Lx)])
 
 print("Complete")
