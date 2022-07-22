5a79d51aae49f71ddde6a988587eae87d4329d06
Tom <thomasjackfairhall@gmail.com>
2022-07-22 15:18:02 +1000

    Updated argument parser

--- main.py
+++ main.py
@@ -2,18 +2,19 @@
 # CSV file order: date, time, temp, pressure, humidity, lux
 # Cron Job running every 30 mins: */30 * * * * /usr/bin/python3 /home/admin/Documents/controller/main.py
 
+import argparse
 import csv
-import sys
 from datetime import datetime
 from PiicoDev_BME280 import PiicoDev_BME280
 from PiicoDev_VEML6030 import PiicoDev_VEML6030
 
 print("Starting...")
 
-input_argument = ""
-
-if(len(sys.argv) > 1):
-    input_argument = sys.argv[1]
+# initalise the input argument parser
+parser = argparse.ArgumentParser()
+parser.add_argument("-r", "--read", help="read measurments", action="store_true")
+parser.add_argument("-w", "--write", help="write measurments", action="store_true")
+args = parser.parse_args()
 
 # initalise the sensors
 bme280 = PiicoDev_BME280()
@@ -33,9 +34,9 @@ pres_HPa = pres_Pa / 100
 now = datetime.now()
 
 # manual or automatic output of environmnetal varaibles
-if(input_argument in ("r", "read")):
+if(args.read):
     print(now.strftime("%Y-%m-%d"), now.strftime("%H:%M:%S"), str(temp_C), str(pres_HPa), str(hum_RH), str(light_Lx))
-else:
+elif(args.write):
     # open, or create a file in append mode and write the environmental varaibles to a cvs file
     with open('/home/admin/Documents/controller/data.csv', 'a', newline='') as file:
         writer = csv.writer(file)
