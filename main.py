f43caf8fda35ee013628c7c4ce16b3c47dc61964
tomfairhall <thomasjackfairhall@gmail.com>
2023-02-17 23:26:07 +1100

    put things in funcitons for portability

--- main.py
+++ main.py
@@ -5,15 +5,12 @@
 # CSV file order: date-time, temp, pressure, humidity, lux
 # Cron Job running every 30 mins: */30 * * * * /usr/bin/python3 /home/admin/Documents/controller/main.py
 
-#TODO: Integrate camera control (currently run seperatly)
-
 import argparse
 import csv
 from statistics import mean
 from datetime import datetime
 from PiicoDev_BME280 import PiicoDev_BME280
 from PiicoDev_VEML6030 import PiicoDev_VEML6030
-
 
 # initialize the input argument parser
 parser = argparse.ArgumentParser()
@@ -25,38 +22,48 @@ parser.add_argument("-w", "--write", help="write measurements to file", action="
 # parse input arguments
 args = parser.parse_args()
 
-# get the current date and time
-now = datetime.now()
+def initilse_data():
+    # get the current date and time
+    now = datetime.now()
 
-# initialise the sensors
-bme280 = PiicoDev_BME280()
-veml6030 = PiicoDev_VEML6030()
+    # initialise the sensors
+    bme280 = PiicoDev_BME280()
+    veml6030 = PiicoDev_VEML6030()
 
-# read and assign initial altitude reading
-zero_alt = bme280.altitude()
+    # read and assign initial altitude reading
+    zero_alt = bme280.altitude()
 
-# initialise sensor value lists
-temp_C_values = []
-pres_HPa_values = []
-hum_RH_values = []
-light_Lx_values = []
+    return now, bme280, veml6030
 
-#! averaging e.g., temp data 3 times might make the board heat up to skew measurements higher...
-for x in range(3):
-    # read and assign the sensor values !! rem to div by 100
-    temp_C, pres_Pa, hum_RH = bme280.values()
-    light_Lx = veml6030.read()
+def measure_data(sample_size, bme280:PiicoDev_BME280, veml6030:PiicoDev_VEML6030):
+        
+    # initialise sensor value lists
+    temp_C_values = []
+    pres_HPa_values = []
+    hum_RH_values = []
+    light_Lx_values = []
 
-    temp_C_values.append(temp_C)
-    pres_HPa_values.append(pres_Pa/100)
-    hum_RH_values.append(hum_RH)
-    light_Lx_values.append(light_Lx)
+    for x in range(sample_size):
+        # read and assign the sensor values !! rem to div by 100
+        temp_C, pres_Pa, hum_RH = bme280.values()
+        light_Lx = veml6030.read()
 
-# find average of measurement values
-temp_C_ave = round(mean(temp_C_values), 2)
-pres_HPa_ave = round(mean(pres_HPa_values), 2)
-hum_RH_ave = round(mean(hum_RH_values), 2)
-light_Lx_ave = round(mean(light_Lx_values), 2)
+        temp_C_values.append(temp_C)
+        pres_HPa_values.append(pres_Pa/100)
+        hum_RH_values.append(hum_RH)
+        light_Lx_values.append(light_Lx)
+    
+    # find average of measurement values
+    temp_C_ave = round(mean(temp_C_values), 2)
+    pres_HPa_ave = round(mean(pres_HPa_values), 2)
+    hum_RH_ave = round(mean(hum_RH_values), 2)
+    light_Lx_ave = round(mean(light_Lx_values), 2)
+
+    return temp_C_ave, pres_HPa_ave, hum_RH_ave, light_Lx_ave    
+
+now, bme280, veml6030 = initilse_data()
+
+temp_C_ave, pres_HPa_ave, hum_RH_ave, light_Lx_ave = measure_data(3, bme280, veml6030)
 
 # output handling
 if(args.read):
