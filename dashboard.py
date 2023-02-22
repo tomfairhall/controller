8e31cc59f1e5372e7f0c70ee586f1cbe4908d383
tomfairhall <thomasjackfairhall@gmail.com>
2023-02-22 19:24:34 +1100

    fix

--- dashboard.py
+++ dashboard.py
@@ -21,24 +21,19 @@ light_Lx_ave = 0
 app = Flask(__name__)
 
 # If the web server is connected to show the main page
-@app.route('/')
+@app.route('/', '/request_data')
 def index():
-
-    return render_template('index.html')
-
-# If the request data button is click, sensors will measure and upload data to screen.
-@app.route('/request_data')
-def request_data():
 
     date_time, temp_C_ave, pres_HPa_ave, hum_RH_ave, light_Lx_ave = controller.measure_data()
 
     return render_template(
         'index.html',
-        date_time=date_time,
-        temperature=temp_C_ave,
-        pressure=pres_HPa_ave,
-        humidity=hum_RH_ave,
-        lux=light_Lx_ave)
+        date_time = date_time,
+        temperature = temp_C_ave,
+        pressure = pres_HPa_ave,
+        humidity = hum_RH_ave,
+        lux = light_Lx_ave,
+        logging_status = logging_status_dictionary[check_logging_status()])
 
 # if download button is clicked, the CSV file will download
 @app.route('/download_data')
