2b7ba98ec1c55c2b00b921d779062f515f873052
tomfairhall <thomasjackfairhall@gmail.com>
2023-02-25 12:23:58 +1100

    delete functionality

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, send_file, redirect, url_for
 from crontab import CronTab, CronItem
-from os import getlogin, remove
+from os import getlogin, remove, path
 
 try:
     from controller import measure_data, DATA_FILE_PATH
@@ -25,16 +25,21 @@ def index():
         pressure = pres_HPa_ave,
         humidity = hum_RH_ave,
         lux = light_Lx_ave,
-        logging_ability = job.is_enabled())
+        logging_ability = job.is_enabled(),
+        file_exists = find_data_file())
+
+def find_data_file():
+    return path.exists(DATA_FILE_PATH)
 
 # If download button is clicked, the CSV file will be download.
 @app.route('/download_data')
 def download_data():
-
+    
     return send_file(
-       DATA_FILE_PATH,
-       as_attachment=True
-    )
+    DATA_FILE_PATH,
+    as_attachment=True)
+
+
 
 # If delete data button is clicked, the CSV file will be deleted.
 @app.route('/delete_data')
