4de000ba4b96ee7f3ebe40ecfad640de505faac7
tomfairhall <thomasjackfairhall@gmail.com>
2023-04-14 15:29:40 +1000

    add link quality output

--- dashboard.py
+++ dashboard.py
@@ -16,6 +16,7 @@ def index():
 
     job, _ = find_logging_job()
     date_time, temp_C_ave, pres_HPa_ave, hum_RH_ave, light_Lx_ave = measure_data()
+    wifi_quality, wifi_strength = find_connection_strength()
 
     return render_template(
         'index.html',
@@ -26,7 +27,8 @@ def index():
         lux = light_Lx_ave,
         logging_ability = job.is_enabled(),
         file_exists = find_data_file(),
-        wifi_strength = find_connection_strength())
+        wifi_quality = wifi_quality,
+        wifi_strength = wifi_strength)
 
 # Check that data file exists.
 def find_data_file():
@@ -34,18 +36,24 @@ def find_data_file():
 
 # Check current WiFi connection strength.
 def find_connection_strength():
-    signal_level_start = "Signal level="
-    signal_level_end   = " dBm"
+    link_start = "Link Quality="
+    link_end = "/70"
+    signal_start = "Signal level="
+    signal_end   = " dBm"
 
     result = run(["iwconfig","wlan0"], text=True, capture_output=True)
 
-    signal_level_index_start = result.stdout.find(signal_level_start)
-    signal_level_index_end  = result.stdout.find(signal_level_end, signal_level_index_start)
+    link_index_start = result.stdout.find(link_start)
+    link_index_end = result.stdout.find(link_end, link_index_end)
+    signal_index_start = result.stdout.find(signal_start)
+    signal_index_end = result.stdout.find(signal_end, signal_index_start)
 
-    index_start = signal_level_index_start+len(signal_level_start)
-    index_end   = signal_level_index_end
+    index_link_start = link_index_start+len(link_start)
+    index_link_end = link_index_end
+    index_signal_start = signal_index_start+len(signal_start)
+    index_signal_end = signal_index_end
 
-    return(int(result.stdout[index_start:index_end]))
+    return(int(result.stdout[index_link_start:index_link_end]), int(result.stdout[index_signal_start:index_signal_end]))
 
 # If download button is clicked, the CSV file will be download.
 @app.route('/download_data')
