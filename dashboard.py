d69026076bd887393bcc667a54e7fc26c20f2e6c
tomfairhall <thomasjackfairhall@gmail.com>
2023-04-14 10:58:28 +1000

    added controller infomation

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, send_file, redirect, url_for
 from crontab import CronTab, CronItem
 from os import getlogin, remove, path
+import subprocess
 
 try:
     from controller import measure_data, DATA_FILE_PATH
@@ -9,12 +10,14 @@ except PermissionError:
 
 app = Flask(__name__)
 
+
 # Main page.
 @app.route('/')
 def index():
 
     job, _ = find_logging_job()
     date_time, temp_C_ave, pres_HPa_ave, hum_RH_ave, light_Lx_ave = measure_data()
+    wifi_strength = find_connection_strength()
 
     return render_template(
         'index.html',
@@ -24,11 +27,28 @@ def index():
         humidity = hum_RH_ave,
         lux = light_Lx_ave,
         logging_ability = job.is_enabled(),
-        file_exists = find_data_file())
+        file_exists = find_data_file(),
+        wifi_strength = wifi_strength)
 
-# Check that data file exists
+# Check that data file exists.
 def find_data_file():
     return path.exists(DATA_FILE_PATH)
+
+# Check current WiFi connection strength.
+def find_connection_strength():
+    signal_level_start = "Signal level="
+    signal_level_end   = " dBm"
+
+    result = subprocess.run(["iwconfig","wlan0"], text=True, capture_output=True)
+
+    signal_level_index_start = result.stdout.find(signal_level_start)
+    signal_level_index_end  = result.stdout.find(signal_level_end, signal_level_index_start)
+
+    index_start = signal_level_index_start+len(signal_level_start)
+    index_end   = signal_level_index_end
+
+    return(int(result.stdout[index_start:index_end]))
+
 
 # If download button is clicked, the CSV file will be download.
 @app.route('/download_data')
@@ -53,7 +73,6 @@ def find_logging_job():
 
     for job in cron.find_command('controller.py -w'):
         return job, cron
-
 
 # If Start/Stop Logging button is clicked, the logging cron job will be enabled/disabled.
 @app.route('/logging_ability')
