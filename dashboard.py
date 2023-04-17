d519f23190f0d1f6efd4c89520b258f9183c0441
tomfairhall <thomasjackfairhall@gmail.com>
2023-04-17 19:29:23 +1000

    test

--- dashboard.py
+++ dashboard.py
@@ -11,7 +11,7 @@ except PermissionError:
 
 app = Flask(__name__)
 
-VERSION = "2"
+VERSION = "abc"
 date_time = temp_C_ave = pres_HPa_ave = hum_RH_ave = light_Lx_ave = 0
 debug_output = debug_error = ""
 
