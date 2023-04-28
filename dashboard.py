72743a8f9e5ee5588f3dbd87d1769764e7267d58
tomfairhall <thomasjackfairhall@gmail.com>
2023-04-28 10:28:26 +1000

    test

--- dashboard.py
+++ dashboard.py
@@ -93,15 +93,10 @@ def query_database(query, args=(), one=False):
 
 def database_to_csv():
     column_names = query_database('SELECT name FROM PRAGMA_TABLE_INFO(\'measurements\')')
-
-    print("here")
-
-    for name in column_names:
-        print(name[1])
-
     rows = query_database('SELECT * FROM measurements')
     with open(CSV_FILE_PATH, mode='w', newline='') as file:
         writer = csv.writer(file)
+        writer.writerow([name for name in column_names])
         writer.writerows(rows)
 
 @app.route('/download_data')
