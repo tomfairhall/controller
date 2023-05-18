c018001d788cd87c590d933b564f556559792ff4
tomfairhall <thomasjackfairhall@gmail.com>
2023-05-18 21:28:59 +1000

    remove server LED display

--- application/__init__.py
+++ application/__init__.py
@@ -1,17 +1,12 @@
 from flask import Flask
 from application import database
-from controller import Display
 
 def init_app():
     app = Flask(__name__)
 
     database.init_app(app)
-    display = Display(mode='s')
-
-    app.teardown_appcontext(display.__exit__(None, None, None))
 
     with app.app_context():
         from . import routes
         database.init_database()
-        display.__enter__()
         return app
