a4b6aa10226bda296640cbbd7a41837e68c02489
tomfairhall <thomasjackfairhall@gmail.com>
2023-05-21 20:47:12 +1000

    remove this again

--- application/__init__.py
+++ application/__init__.py
@@ -2,24 +2,12 @@ from flask import Flask
 from application import database
 from controller import Display
 
-# hacky
-display = Display(mode='s')
-
-def enter():
-    display.__enter__()
-
-def exit():
-    display.__exit__(None, None, None)
-
 def init_app():
     app = Flask(__name__)
 
     database.init_app(app)
 
-    app.teardown_appcontext(exit)
-
     with app.app_context():
         from . import routes
         database.init_database()
-        enter()
         return app
