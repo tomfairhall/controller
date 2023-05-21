cdae7af6a59e5628fe1d56c5ac5b8682eda812ee
tomfairhall <thomasjackfairhall@gmail.com>
2023-05-21 22:22:26 +1000

    test display in new file

--- application/__init__.py
+++ application/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from application import database
+from display import Display
 
 def init_app():
     app = Flask(__name__)
 
     database.init_app(app)
+    Display.init_app(app)
 
     with app.app_context():
         from . import routes
