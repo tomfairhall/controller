ea983f6a5f4b42bd95c5e4ee059a84bc5388672f
tomfairhall <thomasjackfairhall@gmail.com>
2023-05-21 22:27:20 +1000

    add display object instead of using class

--- application/__init__.py
+++ application/__init__.py
@@ -6,7 +6,8 @@ def init_app():
     app = Flask(__name__)
 
     database.init_app(app)
-    Display.init_app(app)
+    display = Display(mode='s')
+    display.init_app(app)
 
     with app.app_context():
         from . import routes
