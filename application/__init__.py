c1c01a3d22f18c0fbf8a17bf16696232072ff962
tomfairhall <thomasjackfairhall@gmail.com>
2023-05-21 20:23:48 +1000

    test

--- application/__init__.py
+++ application/__init__.py
@@ -2,13 +2,16 @@ from flask import Flask
 from application import database
 from controller import Display
 
+def exit(display: Display):
+    display.__exit__(None, None, None)
+
 def init_app():
     app = Flask(__name__)
 
     database.init_app(app)
     display = Display(mode='s')
 
-    app.teardown_appcontext(display.__exit__)
+    app.teardown_appcontext(lambda: display.__exit__(None, None, None))
 
     with app.app_context():
         from . import routes
