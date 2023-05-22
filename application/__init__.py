784fbe8fa80a39779eae2118155523b700c35bc4
tomfairhall <thomasjackfairhall@gmail.com>
2023-05-22 21:05:37 +1000

    test

--- application/__init__.py
+++ application/__init__.py
@@ -2,16 +2,19 @@ from flask import Flask
 from application import database
 from display import Display
 
-def init_app():
+def create_app():
     app = Flask(__name__)
 
     database.init_app(app)
-    display = Display(mode='s')
+    # display = Display(mode='s')
     # display.init_app(app)
+    with app.app_context():
+        from . import routes
+        database.init_database()
+        # display.init_display()
+        return app
+
+def init_app():
 
     with Display(mode='s'):
-        with app.app_context():
-            from . import routes
-            database.init_database()
-            # display.init_display()
-            return app
+        create_app()
