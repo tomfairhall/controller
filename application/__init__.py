7bbeb55d1087cfa00fe0e83f6231e2b80f45b048
tomfairhall <thomasjackfairhall@gmail.com>
2023-05-22 21:20:03 +1000

    test display not using context manager

--- application/__init__.py
+++ application/__init__.py
@@ -1,7 +1,17 @@
 from flask import Flask
 from application import database
+from display import Display
+import atexit
+
+display = Display(mode='s')
+
+def exit_app():
+    display.__exit__(None, None, None)
 
 def init_app():
+    display.__enter__()
+    atexit.register(exit_app)
+
     app = Flask(__name__)
 
     database.init_app(app)
